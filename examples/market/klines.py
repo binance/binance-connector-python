a1b4cbbaefd4454b21eab0c447b7f9503e11947a
Liang Shi <liang.shi@protonmail.com>
2020-05-04 16:22:12 +1000

    fix market example data

--- examples/market/klines.py
+++ examples/market/klines.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
 import logging
-import binance
+from binance.market import Market
 from binance.lib.utils import config_logging
 
 config_logging(logging, logging.DEBUG)
 
-market_client= binance.Market(base_url='https://testnet.binance.vision')
+market_client = Market(base_url='https://testnet.binance.vision')
 
 logging.info(market_client.klines('BTCUSDT', '1m'))
 logging.info(market_client.klines('BTCUSDT', '1h',  limit=10))
