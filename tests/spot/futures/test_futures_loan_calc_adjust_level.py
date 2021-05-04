99484b9096a136e86ecc4d8ae1f61d24f94f1cfe
ishuen <12228644+ishuen@users.noreply.github.com>
2021-05-04 15:23:46 +0800

    update futures endpoints:
        * GET /sapi/v2/futures/loan/wallet to support BUSD loan query
        * GET /sapi/v2/futures/loan/configs to support BUSD loan query
        * GET /sapi/v2/futures/loan/calcAdjustLevel to support BUSD loan
        * GET /sapi/v2/futures/loan/calcMaxAdjustAmount to support adjustment of BUSD loan
        * POST /sapi/v2/futures/loan/adjustCollateral to support adjustment of BUSD loan
        * GET /sapi/v1/futures/loan/adjustCollateral/history new parameter and fields in response loanCoin for BUSD loan
        * GET /sapi/v1/futures/loan/liquidationHistory new parameter and fields in response loanCoin for BUSD loan

--- tests/spot/futures/test_futures_loan_calc_adjust_level.py
+++ tests/spot/futures/test_futures_loan_calc_adjust_level.py
@@ -1,61 +1,40 @@
 import responses
+import pytest
 
 from urllib.parse import urlencode
 from tests.util import random_str
 from tests.util import mock_http_response
 from binance.spot import Spot as Client
-from binance.error import ParameterRequiredError, ClientError
+from binance.error import ParameterRequiredError
 
 mock_item = {'key_1': 'value_1', 'key_2': 'value_2'}
-mock_exception = {'code': -1105, 'msg': 'error message.'}
 
 key = random_str()
 secret = random_str()
 
 params = {
+    'loanCoin': 'BNB',
     'collateralCoin': 'BTC',
     'amount': '1',
     'direction': 'ADDITIONAL',
 }
 
+parameterized_test_data = [
+    ({'loanCoin': '', 'collateralCoin': 'BTC', 'amount': '1', 'direction': 'ADDITIONAL'}),
+    ({'loanCoin': 'BNB', 'collateralCoin': '', 'amount': '1', 'direction': 'ADDITIONAL'}),
+    ({'loanCoin': 'BNB', 'collateralCoin': 'BTC', 'amount': '', 'direction': 'ADDITIONAL'}),
+    ({'loanCoin': 'BNB', 'collateralCoin': 'BTC', 'amount': '1', 'direction': ''})
+]
 
-def test_futures_loan_calc_adjust_level_without_collateralCoin():
-    """ Tests the API endpoint to get adjust level without collateralCoin """
 
-    params = {
-        'collateralCoin': '',
-        'amount': '1',
-        'direction': 'ADDITIONAL',
-    }
+@pytest.mark.parametrize("params", parameterized_test_data)
+def test_futures_loan_calc_adjust_level_with_missing_field(params):
+    """ Tests the API endpoint to get adjust level with missing mandatory field """
     client = Client(key, secret)
     client.futures_loan_calc_adjust_level.when.called_with(**params).should.throw(ParameterRequiredError)
 
 
-def test_futures_loan_calc_adjust_level_without_amount():
-    """ Tests the API endpoint to get adjust level without amount """
-
-    params = {
-        'collateralCoin': 'BTC',
-        'amount': '',
-        'direction': 'ADDITIONAL',
-    }
-    client = Client(key, secret)
-    client.futures_loan_calc_adjust_level.when.called_with(**params).should.throw(ParameterRequiredError)
-
-
-def test_futures_loan_calc_adjust_level_without_direction():
-    """ Tests the API endpoint to get adjust level without direction """
-
-    params = {
-        'collateralCoin': 'BTC',
-        'amount': '1',
-        'direction': '',
-    }
-    client = Client(key, secret)
-    client.futures_loan_calc_adjust_level.when.called_with(**params).should.throw(ParameterRequiredError)
-
-
-@mock_http_response(responses.GET, '/sapi/v1/futures/loan/calcAdjustLevel\\?' + urlencode(params), mock_item, 200)
+@mock_http_response(responses.GET, '/sapi/v2/futures/loan/calcAdjustLevel\\?' + urlencode(params), mock_item, 200)
 def test_futures_loan_calc_adjust_level():
     """ Tests the API endpoint to get adjust level """
 
