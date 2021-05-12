ddce0bf79b8a5adebf60acd5796f3c3368a75b9f
Liang Shi <liang.shi@protonmail.com>
2021-05-12 22:37:26 +1000

    upgrade package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='binance-connector-python',
-    version='0.5.4',
+    version='0.6.0',
     license='MIT',
     description='This is a thin library that working as a connector to the Binance public API.',
     long_description=long_description,
