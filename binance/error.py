f223d5dfe20b34033ae05e023f3255d38f432973
Aisling <rishineal123@gmail.com>
2021-05-12 12:26:50 +0100

    Add header to ClientError (#52)
    * fix typo
    
    * add response header to ClientError
    
    * re-order ClientError parameters

--- binance/error.py
+++ binance/error.py
@@ -3,9 +3,10 @@ class Error(Exception):
 
 
 class ClientError(Error):
-    def __init__(self, status_code, message):
+    def __init__(self, status_code, message, header):
         self.status_code = status_code
         self.message = message
+        self.header = header
 
 
 class ServerError(Error):
