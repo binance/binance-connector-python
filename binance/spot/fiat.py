595588a567e05ca469b1b27dae973ae568f1cc1b
Liang Shi <liang.shi@protonmail.com>
2021-07-30 20:35:15 +1000

    add fiat order history endpoint

---
+++ binance/spot/fiat.py
@@ -0,0 +1,40 @@
+def fiat_order_history(self, transactionType: int, **kwargs):
+    """Get Fiat Deposit/Withdraw History (USER_DATA)
+
+    GET /sapi/v1/fiat/orders
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-fiat-deposit-withdraw-history-user_data
+
+    Args:
+      transactionType (int): 0-deposit,1-withdraw
+    Keyword Args:
+      beginTime (int, optional)
+      endTime (int, optional)
+      page (int, optional): default 1
+      rows (int, optional): default 100, max 500
+      recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    payload = {"transactionType": transactionType, **kwargs}
+    return self.sign_request("GET", "/sapi/v1/fiat/orders", payload)
+
+
+def fiat_payment_history(self, transactionType: int, **kwargs):
+    """Get Fiat Payments History (USER_DATA)
+
+    GET /sapi/v1/fiat/payments
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-fiat-payments-history-user_data
+
+    Args:
+      transactionType (int): 0-buy,1-sell
+    Keyword Args:
+      beginTime (int, optional)
+      endTime (int, optional)
+      page (int, optional): default 1
+      rows (int, optional): default 100, max 500
+      recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    payload = {"transactionType": transactionType, **kwargs}
+    return self.sign_request("GET", "/sapi/v1/fiat/payments", payload)
---
+++ tests/spot/fiat/test_fiat_order_history.py
@@ -0,0 +1,34 @@
+import responses
+
+from tests.util import random_str, timestamp
+from tests.util import mock_http_response
+from urllib.parse import urlencode
+from binance.spot import Spot as Client
+from binance.error import ParameterRequiredError, ClientError
+
+mock_item = {"key_1": "value_1", "key_2": "value_2"}
+
+key = random_str()
+secret = random_str()
+
+transactionType = 0
+
+params = {
+    "beginTime": timestamp(),
+    "endTime": timestamp(),
+    "page": 1,
+    "rows": 100,
+}
+
+
+@mock_http_response(
+    responses.GET,
+    "/sapi/v1/fiat/orders\\?transactionType=0&" + urlencode(params),
+    mock_item,
+    200,
+)
+def test_fiat_order_history():
+    """Tests the API endpoint to get fiat order history"""
+
+    client = Client(key, secret)
+    response = client.fiat_order_history(transactionType, **params)
