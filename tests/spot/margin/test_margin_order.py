1c298073bc1cbfc779223b7b489c9bec33d1f459
ishuen <12228644+ishuen@users.noreply.github.com>
2021-08-02 22:23:31 +0800

    fixing tests

--- tests/spot/margin/test_margin_order.py
+++ tests/spot/margin/test_margin_order.py
@@ -4,7 +4,7 @@ from binance.spot import Spot as Client
 from tests.util import random_str
 from urllib.parse import urlencode
 from tests.util import mock_http_response
-from binance.error import ParameterRequiredError, ClientError
+from binance.error import ParameterRequiredError
 
 mock_item = {"key_1": "value_1", "key_2": "value_2"}
 mock_exception = {"code": -1, "msg": "error message"}
