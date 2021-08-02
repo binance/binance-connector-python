7fa8ac9634c01e38f7139fef0eb3cbfbc39985e8
ishuen <12228644+ishuen@users.noreply.github.com>
2021-08-02 22:42:25 +0800

    enable parameter checks and increase test coverage for fiat endpoints

--- tests/spot/fiat/test_fiat_payment_history.py
+++ tests/spot/fiat/test_fiat_payment_history.py
@@ -4,15 +4,15 @@ from tests.util import random_str, timestamp
 from tests.util import mock_http_response
 from urllib.parse import urlencode
 from binance.spot import Spot as Client
+from binance.error import ParameterRequiredError
 
 mock_item = {"key_1": "value_1", "key_2": "value_2"}
 
 key = random_str()
 secret = random_str()
 
-transactionType = 0
-
 params = {
+    "transactionType": 0,
     "beginTime": timestamp(),
     "endTime": timestamp(),
     "page": 1,
@@ -20,9 +20,18 @@ params = {
 }
 
 
+def test_fiat_payment_history_without_type():
+    """Tests the API endpoint to get fiat order history"""
+
+    client = Client(key, secret)
+    client.fiat_payment_history.when.called_with("").should.throw(
+        ParameterRequiredError
+    )
+
+
 @mock_http_response(
     responses.GET,
-    "/sapi/v1/fiat/payments\\?transactionType=0&" + urlencode(params),
+    "/sapi/v1/fiat/payments\\?" + urlencode(params),
     mock_item,
     200,
 )
@@ -30,5 +39,5 @@ def test_fiat_payment_history():
     """Tests the API endpoint to get fiat payments history"""
 
     client = Client(key, secret)
-    response = client.fiat_payment_history(transactionType, **params)
+    response = client.fiat_payment_history(**params)
     response.should.equal(mock_item)
