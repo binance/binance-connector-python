8f28bdf1c2c6bc9b4a1bf5847c235659723bebea
Jake-Ephraim <ephraimakolo2017@gmail.com>
2023-07-19 19:05:59 +0100

    implemented async request

---
+++ binance/async_spot/_portfolio_margin.py
@@ -0,0 +1,67 @@
+async def portfolio_margin_account(self, **kwargs):
+    """Get Portfolio Margin Account Info (USER_DATA)
+
+    GET /sapi/v1/portfolio/account
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-portfolio-margin-account-info-user_data
+
+    Keyword Args:
+      recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    return await self.sign_request("GET", "/sapi/v1/portfolio/account", {**kwargs})
+
+
+async def portfolio_margin_collateral_rate(self):
+    """Portfolio Margin Collateral Rate (MARKET_DATA)
+
+    Portfolio Margin Collateral Rate.
+
+    Weight(IP): 50
+
+    GET /sapi/v1/portfolio/collateralRate
+
+    https://binance-docs.github.io/apidocs/spot/en/#portfolio-margin-collateral-rate-market_data
+
+    """
+
+    url_path = "/sapi/v1/portfolio/collateralRate"
+    return await self.sign_request("GET", url_path)
+
+
+async def portfolio_margin_bankruptcy_loan_amount(self, **kwargs):
+    """Query Portfolio Margin Bankruptcy Loan Amount (USER_DATA)
+
+    Query Portfolio Margin Bankruptcy Loan Amount.
+
+    Weight(UID): 500
+
+    GET /sapi/v1/portfolio/pmLoan
+
+    https://binance-docs.github.io/apidocs/spot/en/#query-portfolio-margin-bankruptcy-loan-amount-user_data
+
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    url_path = "/sapi/v1/portfolio/pmLoan"
+    return await self.sign_request("GET", url_path, {**kwargs})
+
+
+async def portfolio_margin_bankruptcy_loan_repay(self, **kwargs):
+    """Portfolio Margin Bankruptcy Loan Repay (USER_DATA)
+
+    Repay Portfolio Margin Bankruptcy Loan.
+
+    Weight(UID): 3000
+
+    POST /sapi/v1/portfolio/repay
+
+    https://binance-docs.github.io/apidocs/spot/en/#portfolio-margin-bankruptcy-loan-repay-user_data
+
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    url_path = "/sapi/v1/portfolio/repay"
+    return await self.sign_request("POST", url_path, {**kwargs})
---
+++ binance/async_spot/_savings.py
@@ -0,0 +1,283 @@
+from binance.lib.utils import check_required_parameter
+from binance.lib.utils import check_required_parameters
+
+
+async def savings_flexible_products(self, **kwargs):
+    """Get Flexible Product List (USER_DATA)
+
+    GET /sapi/v1/lending/daily/product/list
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-flexible-product-list-user_data
+
+    Keyword Args:
+        status (str, optional): "ALL", "SUBSCRIBABLE", "UNSUBSCRIBABLE"; default "ALL"
+        featured (str, optional): "ALL", "true"; default "ALL"
+        current (int, optional): Current query page. Default: 1, Min: 1
+        size (int, optional): Default: 50, Max: 100
+        recvWindow (int, optional): The value cannot be greater than 60000
+
+    """
+
+    return await self.sign_request("GET", "/sapi/v1/lending/daily/product/list", kwargs)
+
+
+async def savings_flexible_user_left_quota(self, productId: str, **kwargs):
+    """Get Left Daily Purchase Quota of Flexible Product (USER_DATA)
+
+    GET /sapi/v1/lending/daily/userLeftQuota
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-left-daily-purchase-quota-of-flexible-product-user_data
+
+    Args:
+        productId (str)
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+
+    """
+
+    check_required_parameter(productId, "productId")
+    payload = {"productId": productId, **kwargs}
+    return await self.sign_request("GET", "/sapi/v1/lending/daily/userLeftQuota", payload)
+
+
+async def savings_purchase_flexible_product(self, productId: str, amount: float, **kwargs):
+    """POST Purchase Flexible Product (USER_DATA)
+
+    POST /sapi/v1/lending/daily/purchase
+
+    https://binance-docs.github.io/apidocs/spot/en/#purchase-flexible-product-user_data
+
+    Args:
+        productId (str)
+        amount (float)
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+
+    """
+
+    check_required_parameters([[productId, "productId"], [amount, "amount"]])
+    payload = {"productId": productId, "amount": amount, **kwargs}
+    return await self.sign_request("POST", "/sapi/v1/lending/daily/purchase", payload)
+
+
+async def savings_flexible_user_redemption_quota(self, productId: str, type: str, **kwargs):
+    """Get Left Daily Redemption Quota of Flexible Product (USER_DATA)
+
+    GET /sapi/v1/lending/daily/userRedemptionQuota
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-left-daily-redemption-quota-of-flexible-product-user_data
+
+    Args:
+        productId (str)
+        type (str)
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    check_required_parameters([[productId, "productId"], [type, "type"]])
+    payload = {"productId": productId, "type": type, **kwargs}
+    return await self.sign_request(
+        "GET", "/sapi/v1/lending/daily/userRedemptionQuota", payload
+    )
+
+
+async def savings_flexible_redeem(self, productId: str, amount: float, type: str, **kwargs):
+    """Redeem Flexible Product (USER_DATA)
+
+    POST /sapi/v1/lending/daily/redeem
+
+    https://binance-docs.github.io/apidocs/spot/en/#redeem-flexible-product-user_data
+
+    Args:
+        productId (str)
+        amount (float)
+        type (str)
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    check_required_parameters(
+        [[productId, "productId"], [amount, "amount"], [type, "type"]]
+    )
+    payload = {"productId": productId, "amount": amount, "type": type, **kwargs}
+    return await self.sign_request("POST", "/sapi/v1/lending/daily/redeem", payload)
+
+
+async def savings_flexible_product_position(self, **kwargs):
+    """Get Flexible Product Position (USER_DATA)
+
+    GET /sapi/v1/lending/daily/token/position
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-flexible-product-position-user_data
+
+    Keyword Args:
+        asset (str)
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    return await self.sign_request("GET", "/sapi/v1/lending/daily/token/position", kwargs)
+
+
+async def savings_project_list(self, type: str, **kwargs):
+    """Get Fixed and Activity Project List(USER_DATA)
+
+    GET /sapi/v1/lending/project/list
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-fixed-and-activity-project-list-user_data
+
+    Args:
+        type (str)
+    Keyword Args:
+        asset (str, optional)
+        status (str, optional): "ALL", "SUBSCRIBABLE", "UNSUBSCRIBABLE"; default "ALL"
+        isSortAsc (bool, optional): By default, it's True
+        sortBy (str, optional): "START_TIME", "LOT_SIZE", "INTEREST_RATE", "DURATION"; default "START_TIME"
+        current (int, optional): Currently querying page. Start from 1. Default:1
+        size (int, optional): Default:10, Max:100
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    check_required_parameter(type, "type")
+    payload = {"type": type, **kwargs}
+    return await self.sign_request("GET", "/sapi/v1/lending/project/list", payload)
+
+
+async def savings_purchase_project(self, projectId: str, lot: int, **kwargs):
+    """Purchase Fixed/Activity Project (USER_DATA)
+
+    POST /sapi/v1/lending/customizedFixed/purchase
+
+    https://binance-docs.github.io/apidocs/spot/en/#purchase-fixed-activity-project-user_data
+
+    Args:
+        projectId (str)
+        lot (int)
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    check_required_parameters([[projectId, "projectId"], [lot, "lot"]])
+    payload = {"projectId": projectId, "lot": lot, **kwargs}
+    return await self.sign_request(
+        "POST", "/sapi/v1/lending/customizedFixed/purchase", payload
+    )
+
+
+async def savings_project_position(self, **kwargs):
+    """Get Fixed/Activity Project Position  (USER_DATA)
+
+    GET /sapi/v1/lending/project/position/list
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-fixed-activity-project-position-user_data
+
+    Keyword Args:
+        asset (str, optional)
+        projectId (str, optional)
+        status (str, optional): "HOLDING", "REDEEMED"
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    return await self.sign_request("GET", "/sapi/v1/lending/project/position/list", kwargs)
+
+
+async def savings_account(self, **kwargs):
+    """Lending Account (USER_DATA)
+
+    GET /sapi/v1/lending/union/account
+
+    https://binance-docs.github.io/apidocs/spot/en/#lending-account-user_data
+
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    return await self.sign_request("GET", "/sapi/v1/lending/union/account", kwargs)
+
+
+async def savings_purchase_record(self, lendingType: str, **kwargs):
+    """Get Purchase Record (USER_DATA)
+
+    GET /sapi/v1/lending/union/purchaseRecord
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-purchase-record-user_data
+
+    Args:
+        lendingType (str): "DAILY" for flexible, "ACTIVITY" for activity, "CUSTOMIZED_FIXED" for fixed
+    Keyword Args:
+        asset (str, optional)
+        startTime (int, optional)
+        endTime (int, optional)
+        current (int, optional): Currently querying page. Start from 1. Default:1
+        size (int, optional): Default:10 Max:100
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    check_required_parameter(lendingType, "lendingType")
+    payload = {"lendingType": lendingType, **kwargs}
+    return await self.sign_request("GET", "/sapi/v1/lending/union/purchaseRecord", payload)
+
+
+async def savings_redemption_record(self, lendingType: str, **kwargs):
+    """Get Redemption Record (USER_DATA)
+
+    GET /sapi/v1/lending/union/redemptionRecord
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-redemption-record-user_data
+
+    Args:
+        lendingType (str): "DAILY" for flexible, "ACTIVITY" for activity, "CUSTOMIZED_FIXED" for fixed
+    Keyword Args:
+        asset (str, optional)
+        startTime (int, optional)
+        endTime (int, optional)
+        current (int, optional): Currently querying page. Start from 1. Default:1
+        size (int, optional): Default:10 Max:100
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    check_required_parameter(lendingType, "lendingType")
+    payload = {"lendingType": lendingType, **kwargs}
+    return await self.sign_request("GET", "/sapi/v1/lending/union/redemptionRecord", payload)
+
+
+async def savings_interest_history(self, lendingType: str, **kwargs):
+    """Get Interest History (USER_DATA)
+
+    GET /sapi/v1/lending/union/interestHistory
+
+    https://binance-docs.github.io/apidocs/spot/en/#get-interest-history-user_data-2
+
+    Args:
+        lendingType (str): "DAILY" for flexible, "ACTIVITY" for activity, "CUSTOMIZED_FIXED" for fixed
+    Keyword Args:
+        asset (str, optional)
+        startTime (int, optional)
+        endTime (int, optional)
+        current (int, optional): Currently querying page. Start from 1. Default:1
+        size (int, optional): Default:10 Max:100
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    check_required_parameter(lendingType, "lendingType")
+    payload = {"lendingType": lendingType, **kwargs}
+    return await self.sign_request("GET", "/sapi/v1/lending/union/interestHistory", payload)
+
+
+async def savings_change_position(self, projectId: str, lot: int, **kwargs):
+    """Change Fixed/Activity Position to Daily Position(USER_DATA)
+
+    POST /sapi/v1/lending/positionChanged
+
+    https://binance-docs.github.io/apidocs/spot/en/#change-fixed-activity-position-to-daily-position-user_data
+
+    Args:
+        projectId (str)
+        lot (int)
+    Keyword Args:
+        positionId (int, optional): for fixed position
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+
+    check_required_parameters([[projectId, "projectId"], [lot, "lot"]])
+    payload = {"projectId": projectId, "lot": lot, **kwargs}
+    return await self.sign_request("POST", "/sapi/v1/lending/positionChanged", payload)
