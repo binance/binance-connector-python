5960eb68e56b654ce87b1235886b6741c227a950
付煜铭 <solit1106@gmail.com>
2025-01-31 14:35:40 +0800

    Update _staking.py

--- binance/spot/_staking.py
+++ binance/spot/_staking.py
@@ -261,3 +261,232 @@ def get_wbeth_unwrap_history(self, **kwargs):
     """
     url_path = "/sapi/v1/eth-staking/wbeth/history/unwrapHistory"
     return self.sign_request("GET", url_path, {**kwargs})
+
+def sol_staking_account(self, **kwargs):
+    """SOL Staking account (USER_DATA)
+
+    SOL Staking account
+
+    Weight(IP): 150
+
+    GET /sapi/v1/sol-staking/account
+
+    https://developers.binance.com/docs/staking/sol-staking/account
+
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    url_path = "/sapi/v1/sol-staking/account"
+    return self.sign_request("GET", url_path, {**kwargs})
+
+
+def get_sol_staking_quota(self, **kwargs):
+    """Get current SOL staking quota (USER_DATA)
+
+    Get current SOL staking quota
+
+    Weight(IP): 150
+
+    GET /sapi/v1/sol-staking/sol/quota
+
+    https://developers.binance.com/docs/staking/sol-staking/account/Get-current-SOL-staking-quota
+
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    url_path = "/sapi/v1/sol-staking/sol/quota"
+    return self.sign_request("GET", url_path, {**kwargs})
+
+
+def subscribe_sol_staking(self, amount: float, **kwargs):
+    """Subscribe SOL Staking (TRADE)
+
+    Subscribe SOL Staking
+
+    Weight(IP): 150
+
+    POST /sapi/v1/sol-staking/sol/stake
+
+    https://developers.binance.com/docs/staking/sol-staking/staking
+
+    Args:
+        amount (float): Amount in SOL, limit 4 decimals
+
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    check_required_parameter(amount, "amount")
+
+    params = {"amount": amount, **kwargs}
+    url_path = "/sapi/v1/sol-staking/sol/stake"
+    return self.sign_request("POST", url_path, params)
+
+
+def redeem_sol(self, amount: float, **kwargs):
+    """Redeem SOL (TRADE)
+
+    Redeem BNSOL and get SOL
+
+    Weight(IP): 150
+
+    POST /sapi/v1/sol-staking/sol/redeem
+
+    https://developers.binance.com/docs/staking/sol-staking/staking/Redeem-SOL
+
+    Args:
+        amount (float): Amount in BNSOL, limit 8 decimals
+
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    check_required_parameter(amount, "amount")
+
+    params = {"amount": amount, **kwargs}
+    url_path = "/sapi/v1/sol-staking/sol/redeem"
+    return self.sign_request("POST", url_path, params)
+
+
+def claim_boost_rewards(self, **kwargs):
+    """Claim Boost Rewards (TRADE)
+
+    Claim Boost APR Airdrop Rewards 
+
+    Weight(IP): 150
+
+    POST /sapi/v1/sol-staking/sol/claim
+
+    https://developers.binance.com/docs/staking/sol-staking/staking/Claim-Boost-rewards
+
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    url_path = "/sapi/v1/sol-staking/sol/claim"
+    return self.sign_request("POST", url_path, {**kwargs})
+
+
+def get_sol_staking_history(self, **kwargs):
+    """Get SOL staking history (USER_DATA)
+
+    Get SOL staking history
+
+    Weight(IP): 150
+
+    GET /sapi/v1/sol-staking/sol/history/stakingHistory
+
+    https://developers.binance.com/docs/staking/sol-staking/history
+
+    Keyword Args:
+        startTime (int, optional)
+        endTime (int, optional)
+        current (int, optional): Currently querying page. Start from 1. Default: 1
+        size (int, optional): Default: 10, Max: 100
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    url_path = "/sapi/v1/sol-staking/sol/history/stakingHistory"
+    return self.sign_request("GET", url_path, {**kwargs})
+
+
+def get_sol_redemption_history(self, **kwargs):
+    """Get SOL redemption history (USER_DATA)
+
+    Get SOL redemption history
+
+    Weight(IP): 150
+
+    GET /sapi/v1/sol-staking/sol/history/redemptionHistory
+
+    https://developers.binance.com/docs/staking/sol-staking/history/Get-SOL-redemption-history
+
+    Keyword Args:
+        startTime (int, optional)
+        endTime (int, optional)
+        current (int, optional): Currently querying page. Start from 1. Default: 1
+        size (int, optional): Default: 10, Max: 100
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    url_path = "/sapi/v1/sol-staking/sol/history/redemptionHistory"
+    return self.sign_request("GET", url_path, {**kwargs})
+
+
+def get_bnsol_rewards_history(self, **kwargs):
+    """Get BNSOL rewards history (USER_DATA)
+
+    Get BNSOL rewards history
+
+    Weight(IP): 150
+
+    GET /sapi/v1/sol-staking/sol/history/bnsolRewardsHistory
+
+    https://developers.binance.com/docs/staking/sol-staking/history/Get-BNSOL-rewards-history
+
+    Keyword Args:
+        startTime (int, optional)
+        endTime (int, optional)
+        current (int, optional): Currently querying page. Start from 1. Default: 1
+        size (int, optional): Default: 10, Max: 100
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    url_path = "/sapi/v1/sol-staking/sol/history/bnsolRewardsHistory"
+    return self.sign_request("GET", url_path, {**kwargs})
+
+
+def get_bnsol_rate_history(self, **kwargs):
+    """Get BNSOL Rate History (USER_DATA)
+
+    Get BNSOL Rate History
+
+    Weight(IP): 150
+
+    GET /sapi/v1/sol-staking/sol/history/rateHistory
+
+    https://developers.binance.com/docs/staking/sol-staking/history/Get-BNSOL-Rate-History
+
+    Keyword Args:
+        startTime (int, optional)
+        endTime (int, optional)
+        current (int, optional): Currently querying page. Start from 1. Default: 1
+        size (int, optional): Default: 10, Max: 100
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    url_path = "/sapi/v1/sol-staking/sol/history/rateHistory"
+    return self.sign_request("GET", url_path, {**kwargs})
+
+
+def get_boost_rewards_history(self, **kwargs):
+    """Get Boost rewards history (USER_DATA)
+
+    Get Boost rewards history
+
+    Weight(IP): 150
+
+    GET /sapi/v1/sol-staking/sol/history/boostRewardsHistory
+
+    https://developers.binance.com/docs/staking/sol-staking/history/Get-Boost-rewards-History
+
+    Keyword Args:
+        startTime (int, optional)
+        endTime (int, optional)
+        current (int, optional): Currently querying page. Start from 1. Default: 1
+        size (int, optional): Default: 10, Max: 100
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    url_path = "/sapi/v1/sol-staking/sol/history/boostRewardsHistory"
+    return self.sign_request("GET", url_path, {**kwargs})
+
+
+def get_unclaimed_rewards(self, **kwargs):
+    """Get Unclaimed Rewards (USER_DATA)
+
+    Get Unclaimed Rewards
+
+    Weight(IP): 150
+
+    GET /sapi/v1/sol-staking/sol/history/unclaimedRewards
+
+    https://developers.binance.com/docs/staking/sol-staking/history/Get-Unclaimed-rewards
+
+    Keyword Args:
+        recvWindow (int, optional): The value cannot be greater than 60000
+    """
+    url_path = "/sapi/v1/sol-staking/sol/history/unclaimedRewards"
+    return self.sign_request("GET", url_path, {**kwargs})
